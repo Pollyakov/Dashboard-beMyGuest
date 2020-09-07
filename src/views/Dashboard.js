eb3722f4106080bcfe7b3e5966e7d6629a8d094f
Ksenia <ksusha.net@gmail.com>
2020-09-08 00:27:34 +0300

    Dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -19,6 +19,10 @@ import {
 } from "variables/charts.js";
 import CardsGallery from "../components/CardsGallery/CardsGallery";
   import './dashboard.css';
+import UsersGraph from "components/UsersGraph/UsersGraph";
+import MealsManagement from "./MealsManagement";
+import UserManagement from "./UserManagement";
+import MealsGraph from "components/UsersGraph/MealsGraph/MealsGraph";
 
 class Dashboard extends React.Component {
   render() {
@@ -28,88 +32,23 @@ class Dashboard extends React.Component {
           <Row className="firstrow">
               <CardsGallery/>
               
-          <Col lg="12" md="12" sm="12">
+          {/* <Col lg="12" md="12" sm="12">
             
-            </Col>
+            </Col> */}
           </Row>
 
+         
           <Row>
-            <Col md="12">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h5">Users Behavior</CardTitle>
-                  <p className="card-category">24 Hours performance</p>
-                </CardHeader>
-                <CardBody>
-                  <Line
-                    data={dashboard24HoursPerformanceChart.data}
-                    options={dashboard24HoursPerformanceChart.options}
-                    width={400}
-                    height={100}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="fa fa-history" /> Updated 3 minutes ago
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-          </Row>
-          <Row>
-            <Col md="4">
-              <Card>
-                <CardHeader>
-                  <CardTitle tag="h5">Email Statistics</CardTitle>
-                  <p className="card-category">Last Campaign Performance</p>
-                </CardHeader>
-                <CardBody>
-                  <Pie
-                    data={dashboardEmailStatisticsChart.data}
-                    options={dashboardEmailStatisticsChart.options}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <div className="legend">
-                    <i className="fa fa-circle text-primary" /> Opened{" "}
-                    <i className="fa fa-circle text-warning" /> Read{" "}
-                    <i className="fa fa-circle text-danger" /> Deleted{" "}
-                    <i className="fa fa-circle text-gray" /> Unopened
-                  </div>
-                  <hr />
-                  <div className="stats">
-                    <i className="fa fa-calendar" /> Number of emails sent
-                  </div>
-                </CardFooter>
-              </Card>
+            <Col md="6">
+                <UserManagement/>
             </Col>
             
-            <Col md="8">
-              <Card className="card-chart">
-                <CardHeader>
-                  <CardTitle tag="h5">NASDAQ: AAPL</CardTitle>
-                  <p className="card-category">Line Chart with Points</p>
-                </CardHeader>
-                <CardBody>
-                  <Line
-                    data={dashboardNASDAQChart.data}
-                    options={dashboardNASDAQChart.options}
-                    width={400}
-                    height={100}
-                  />
-                </CardBody>
-                <CardFooter>
-                  <div className="chart-legend">
-                    <i className="fa fa-circle text-info" /> Tesla Model S{" "}
-                    <i className="fa fa-circle text-warning" /> BMW 5 Series
-                  </div>
-                  <hr />
-                  <div className="card-stats">
-                    <i className="fa fa-check" /> Data information certified
-                  </div>
-                </CardFooter>
-              </Card>
+            <Col md="6">
+              {/* <Card className="card-chart"> */}
+               <MealsGraph/>
+               <UsersGraph/>
+             
+              {/* </Card> */}
             </Col>
           </Row>
         </div>
--- src/views/UserManagement.js
+++ src/views/UserManagement.js
@@ -1,64 +1,64 @@
 import React, { Component } from "react";
 import { Container, Table, Col, Button } from "react-bootstrap";
-import BootstrapTable from 'react-bootstrap-table-next';
+import BootstrapTable from "react-bootstrap-table-next";
 import axios from "axios";
-import './UserManagement.css';
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import "./UserManagement.css";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 // reactstrap components
 import {
   Card,
   CardHeader,
   CardBody,
   CardTitle,
-//   Table,
+  //   Table,
   Row,
-//   Col,
+  //   Col,
 } from "reactstrap";
 
 class UserManagement extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          users: [],
-        };
-      }
-      componentDidMount() {
-        axios
-          .get("https://tabsur.herokuapp.com/api/system/users")
-          .then((response) => {
-            this.setState({
-              users: response.data,
-            });
-          });
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("https://tabsur.herokuapp.com/api/system/users")
+      .then((response) => {
+        this.setState({
+          users: response.data,
+        });
+      });
+  }
 
   render() {
     const columns = [
-        {
-          dataField: "id",
-          text: "user ID",
-          sort: true,
-        },
-        {
-          dataField: "name",
-          text: "User Name",
-          sort: true,
-        },
-        {
-          dataField: "created_at",
-          text: "Creation Date",
-          sort: true,
-        },
-        {
-          dataField: "email",
-          text: "User email",
-          sort: true,
-        },
-      ];
-      const selectRow = {
-        mode: "checkbox",
-        clickToSelect: true,
-      };
+      {
+        dataField: "id",
+        text: "user ID",
+        sort: true,
+      },
+      {
+        dataField: "name",
+        text: "User Name",
+        sort: true,
+      },
+      {
+        dataField: "created_at",
+        text: "Creation Date",
+        sort: true,
+      },
+      {
+        dataField: "email",
+        text: "User email",
+        sort: true,
+      },
+    ];
+    const selectRow = {
+      mode: "checkbox",
+      clickToSelect: true,
+    };
     return (
       <>
         <div className="content">
@@ -66,54 +66,50 @@ class UserManagement extends React.Component {
             <Col md="12">
               <Card>
                 <CardBody>
-                <div className="button-container">
-              <Button
-                href="#"
-                color="primary"
-               
-                className="btn btn-outline-default btn-block delete"
-              >
-                Delete
-              </Button>
-              <Button
-                href="#"
-                color="primary"
-                block
-                className="btn btn-outline-default btn-block"
-              > 
-            
-                Reset Password
-              </Button>
-              <Button
-                href="#"
-                color="primary"
-                block
-                className="btn btn-outline-default btn-block"
-              >
-                Rename
-              </Button>
-              <Button
-                href="#"
-                color="primary"
-                block
-                className="btn btn-outline-default btn-block"
-              >
-                Send Mail
-                {/* <i className="nc-email-85" /> */}
-              </Button>
-              </div>
-              
-                <BootstrapTable
-                 keyField="id"
-                 data={this.state.users}
-                 columns={columns}
-                selectRow={selectRow}
-                 />
-                
+                  <BootstrapTable
+                    keyField="id"
+                    data={this.state.users}
+                    columns={columns}
+                    selectRow={selectRow}
+                  />
+
+                  <div className="button-container">
+                    <Button
+                      href="#"
+                      color="primary"
+                      className="btn btn-outline-default btn-block delete"
+                    >
+                      Delete
+                    </Button>
+                    <Button
+                      href="#"
+                      color="primary"
+                      block
+                      className="btn btn-outline-default btn-block"
+                    >
+                      Reset Password
+                    </Button>
+                    <Button
+                      href="#"
+                      color="primary"
+                      block
+                      className="btn btn-outline-default btn-block"
+                    >
+                      Rename
+                    </Button>
+                    <Button
+                      href="#"
+                      color="primary"
+                      block
+                      className="btn btn-outline-default btn-block"
+                    >
+                      Send Mail
+                      {/* <i className="nc-email-85" /> */}
+                    </Button>
+                  </div>
                 </CardBody>
               </Card>
             </Col>
-            
           </Row>
         </div>
       </>
