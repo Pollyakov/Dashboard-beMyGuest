3a59af0b222f7c8cf3c5fb51f6d4da6683260347
Pollyakov <ksusha.net@gmail.com>
2020-09-07 16:02:54 +0300

    MealsGraph Component

---
+++ src/components/UsersGraph/MealsGraph/MealsGraph.js
@@ -0,0 +1,186 @@
+import React, { Component } from "react";
+import { Line} from "react-chartjs-2";
+import axios from "axios";
+import {
+  Card, CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Row,
+  Col
+} from "reactstrap";
+const chartColor = '#FFFFFF';
+
+class MealsGraph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://tabsur.herokuapp.com/api/system/statsMeals")
+      .then((response) => {
+        this.setState({
+          data: response.data,
+        });
+      });
+  }
+
+  render() {
+    const options = {
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        // tooltips: {
+        //     bodySpacing: 4,
+        //     mode:"nearest",
+        //     intersect: 0,
+        //     position:"nearest",
+        //     xPadding:10,
+        //     yPadding:10,
+        //     caretPadding:10
+        // },
+        responsive: 1,
+        scales: {
+            yAxes: [{ 
+                stepsize: 1,
+                display:1,
+                ticks: {
+                    display: true,
+                    stepSize: 1,
+                },
+                gridLines: {
+                    zeroLineColor: "#ef8157",
+                    drawTicks: true,
+                    display:true,
+                    drawBorder: true, 
+                    stepsize: 0.5,
+                }
+            }],
+            xAxes: [
+              {
+                // type: 'time',
+                time: {stepSize:1},
+                distribution: 'linear',
+                display:1,
+                ticks: {
+                    display: true,
+                    reverse: true,
+                    sourse: 'labels',
+                },
+                gridLines: {
+                    zeroLineColor: "#ef8157",
+                    drawTicks: true,
+                    display: true,
+                    drawBorder: true,
+                }
+            }
+          ]
+        },
+        layout:{
+            padding:{left:0,right:0,top:15,bottom:15}
+        }
+    };
+    const getLabels = () => {
+      if (this.state.data) {
+        return this.state.data.map((element) => {
+          return String(element.days_before)
+        })
+      } else {
+        return [];
+      }
+    }
+    const getNewMeals = () => {
+      if (this.state.data) {
+        return this.state.data.map((element) => {
+          return parseInt(element.mealscreated)
+        })
+      } else {
+        return [];
+      }
+    }
+    const getActiveMeals = () => {
+      if (this.state.data) {
+        return this.state.data.map((element) => {
+          return element.activemeals
+        })
+      }
+      else {
+        return [];
+      }
+    }
+
+    let labels = [];
+    let newMeals = [];
+    let activeMeals = [];
+    if (this.state.data) {
+       labels = getLabels();
+       newMeals = getNewMeals();
+       activeMeals= getActiveMeals();
+    }
+
+    const data = {
+      labels: labels,      
+      datasets: [
+        {
+          data: newMeals,
+          fill: false,
+          borderColor: "#fbc658",
+          backgroundColor: "transparent",
+        //   pointBorderColor: "#fbc658",
+        //   pointRadius: 4,
+        //   pointHoverRadius: 4,
+        //   pointBorderWidth: 8,
+        },
+        {
+          data: activeMeals,
+          fill: false,
+          borderColor: "#51CACF",
+          backgroundColor: "transparent",
+        //   pointBorderColor: "#51CACF",
+        //   pointRadius: 4,
+        //   pointHoverRadius: 4,
+        //   pointBorderWidth: 8,
+        },
+      ],
+    };
+    return (
+      <Row>
+        <Col md="12">
+          <Card className="card-chart">
+            <CardHeader>
+              <CardTitle tag="h5">Meals Per Day</CardTitle>
+              <p className="card-category">New Meals and Active Meals per day</p>
+            </CardHeader>
+            <CardBody>
+              {this.state.data ? 
+              <Line
+                data={data}
+                options={options}
+                width={700}
+                height={200}
+              /> : null}
+
+            </CardBody>
+            <CardFooter>
+              <div className="chart-legend">
+                <i className="fa fa-circle text-warning" /> New Meals {" "}
+                <i className="fa fa-circle text-info" /> Active Meals  {" "}
+              </div>
+              <hr />
+              <div className="card-stats">
+                <i className="fa fa-check" /> Data information certified
+              </div>
+            </CardFooter>
+          </Card>
+        </Col>
+      </Row >
+    );
+
+  }
+}
+export default MealsGraph;
--- src/views/Statistics/StatisticsPage.js
+++ src/views/Statistics/StatisticsPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import UsersGraph from 'components/UsersGraph/UsersGraph';
+import MealsGraph from 'components/UsersGraph/MealsGraph/MealsGraph';
 
 class StatisticsPage extends Component {
     render() {
         return (
             <div className="content">
                 <UsersGraph/>
+                <MealsGraph/>
             </div>
         );
     }
