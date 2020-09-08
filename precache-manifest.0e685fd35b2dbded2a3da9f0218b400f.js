e52c9201884000f7202ddaee96baa36af6a4f847
Ksenia <ksusha.net@gmail.com>
2020-09-08 12:23:51 +0300

    Updates

---
+++ precache-manifest.0e685fd35b2dbded2a3da9f0218b400f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a4ce13e697e646d8a849ac061720d4",
+    "url": "/Dashboard-beMyGuest/index.html"
+  },
+  {
+    "revision": "ba1704293b370c641210",
+    "url": "/Dashboard-beMyGuest/static/css/2.b4be1e4b.chunk.css"
+  },
+  {
+    "revision": "9fea18b13d7c8dd5cd66",
+    "url": "/Dashboard-beMyGuest/static/css/main.78af848d.chunk.css"
+  },
+  {
+    "revision": "ba1704293b370c641210",
+    "url": "/Dashboard-beMyGuest/static/js/2.4ab52022.chunk.js"
+  },
+  {
+    "revision": "02142d79a6544f14147002ff3d7a880a",
+    "url": "/Dashboard-beMyGuest/static/js/2.4ab52022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fea18b13d7c8dd5cd66",
+    "url": "/Dashboard-beMyGuest/static/js/main.eac3b7a9.chunk.js"
+  },
+  {
+    "revision": "7ba0493c964ae5bd1c38",
+    "url": "/Dashboard-beMyGuest/static/js/runtime-main.561e690d.js"
+  },
+  {
+    "revision": "1ec9984b9c2ad8e13003ade91229709f",
+    "url": "/Dashboard-beMyGuest/static/media/logo.1ec9984b.svg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/Dashboard-beMyGuest/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/Dashboard-beMyGuest/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/Dashboard-beMyGuest/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/Dashboard-beMyGuest/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
